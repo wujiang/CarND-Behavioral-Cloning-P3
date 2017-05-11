c6630ce54588f51c204df570ca777467dd383e5f
Wu Jiang <wu@morediff.info>
2017-05-11 13:03:19 -0400

    Use left, right images, cropping images

--- model.py
+++ model.py
@@ -5,34 +5,53 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 
 images = []
 measurements = []
+CORRECTION = 0.2
+IMG_PATH = "./data/IMG/{}"
+
+
+def read_image(path):
+    path = path.split("/")[-1]
+    image = cv2.imread(IMG_PATH.format(path))
+
+
+def flip_image(image):
+    return cv2.flip(image, 1)
+
 
 with open("data/driving_log.csv", "r") as f:
     reader = csv.reader(f)
     for line in reader:
-        source_path = line[0]
-        filename = source_path.split("/")[-1]
-        image = cv2.imread("data/IMG/{}".format(filename))
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
-        images.append(cv2.flip(image, 1))
-        measurements.append(-measurement)
+        center_path, left_path, right_path = line[:3]
+        center_image = read_image(center_path)
+        left_image = read_image(left_path)
+        right_image = read_image(right_path)
+        images.extend([center_image, left_image, right_image])
+
+        center_measurement = float(line[3])
+        left_measurement = center_measurement + CORRECTION
+        right_measurement = center_measurement - CORRECTION
+        measurements.extend([center_measurement, left_measurement, right_measurement])
+
+        images.append(flip_image(image))
+        measurements.append(-1.0 * measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
 INPUT_SHAPE = (160, 320, 3)
+CROPPING = ((70, 25), (0, 0))
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=INPUT_SHAPE))
+model.add(Cropping2D(cropping=CROPPING))
 model.add(Convolution2D(6, 5, 5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation="relu"))
