6a1d6a28ab0b9f622e85432bca178d6a06fd7661
Wu Jiang <wu@morediff.info>
2017-05-14 01:03:34 -0400

    Refactor to use generator

--- model.py
+++ model.py
@@ -4,16 +4,20 @@ import csv
 import cv2
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 
-images = []
-measurements = []
 CORRECTION = 0.1
+DRIVING_LOGS = "./data/driving_log.csv"
 IMG_PATH = "./data/IMG/{}"
+INPUT_SHAPE = (160, 320, 3)
+CROPPING = ((70, 25), (0, 0))
+BATCH_SIZE=32
 
 
 def read_image(path):
@@ -26,32 +30,52 @@ def flip_image(image):
     return cv2.flip(image, 1)
 
 
-with open("data/driving_log.csv", "r") as f:
+def transform_data(samples, batch_size=BATCH_SIZE):
+    """Returns a generator for X_train and y_train
+
+    :param samples: lines from driving_logs.csv
+    :param batch_size: number of samples to return each time
+    """
+    samples_size = len(samples)
+    while True:
+        shuffle(samples)
+        for offset in range(0, samples_size, batch_size):
+            batch_samples = samples[offset: offset + batch_size]
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                center_path, left_path, right_path = batch_sample[:3]
+                clr_images = [read_image(elem) for elem in
+                              [center_path, left_path, right_path]]
+                images.extend(clr_images)
+
+                center_measurement = float(batch_sample[3])
+                clr_measurements = [
+                    center_measurement,
+                    center_measurement + CORRECTION,
+                    center_measurement - CORRECTION,
+                ]
+                measurements.extend(clr_measurements)
+
+                images.extend([flip_image(elem) for elem in clr_images])
+                measurements.extend([-1.0 * elem for elem in clr_measurements])
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield shuffle(X_train, y_train)
+
+
+samples = []
+with open(DRIVING_LOGS, "r") as f:
     reader = csv.reader(f)
     for line in reader:
-        center_path, left_path, right_path = line[:3]
-        center_image = read_image(center_path)
-        left_image = read_image(left_path)
-        right_image = read_image(right_path)
-        images.extend([center_image, left_image, right_image])
+        samples.append(line)
 
-        center_measurement = float(line[3])
-        left_measurement = center_measurement + CORRECTION
-        right_measurement = center_measurement - CORRECTION
-        measurements.extend([center_measurement, left_measurement, right_measurement])
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-        images.append(flip_image(center_image))
-        measurements.append(-1.0 * center_measurement)
-        images.append(flip_image(left_image))
-        measurements.append(-1.0 * left_measurement)
-        images.append(flip_image(right_image))
-        measurements.append(-1.0 * right_measurement)
+train_generator = transform_data(train_samples)
+validation_generator = transform_data(validation_samples)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-INPUT_SHAPE = (160, 320, 3)
-CROPPING = ((70, 25), (0, 0))
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=INPUT_SHAPE))
@@ -69,6 +93,10 @@ model.add(Dense(1))
 
 
 model.compile(loss="mse", optimizer="adam")
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit_generator(train_generator,
+                    samples_per_epoch=len(train_samples),
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples),
+                    nb_epoch=10)
 
 model.save("model.h5")
